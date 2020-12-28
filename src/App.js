7378bded52a244cdfab7b8a4af77db3c3c058aaa
bashar-orlando <obarrong@uni.pe>
2020-12-27 21:06:48 -0500

    Axios_Test v2.0

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import ReactAutocomplete from 'react-autocomplete';
+import {useSearch} from "./hooks";
+
+function App() {
+    const [value, setValue] = useState('');
+    const {articles, status, error} = useSearch(value);
+
+    return (
+      <div>
+        <p>Status: {status}</p>
+        <p>Error: {error}</p>
+        <ReactAutocomplete
+            items = {articles} 
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                getItemValue={item => item.label}
+                renderItem={(item, highlighted) =>
+                  <div
+                    key={item.id}
+                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                  >
+                    {item.label}
+                  </div>
+                }
+                value = {value}
+                onChange = {e => setValue( e.target.value )}
+                onSelect = {value => setValue( value )}
+        />
+      </div>
+    );
+}
+
+export default App;
